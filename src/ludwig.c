0cd0bd5e9377290bc9c7ffd4f6fe746041d9c8cd
Gabriele <gdallava@LAPTOP-24GHADLM.localdomain>
2026-01-21 16:34:06 +0100

    adding time dependent EF: fixato il valore + commento

--- src/ludwig.c
+++ src/ludwig.c
@@ -463,7 +463,10 @@ void ludwig_run(const char * inputfile) {
   }
 
   //MODALITA CAMPO ELETTRICO DIDPENDENTE DAL TEMPO
-  int ATTIVA_EF_t = 1; // 0 = no, 1 = yes
+  int ATTIVA_EF_t = 0; // 0 = no, 1 = yes
+  if (ATTIVA_EF_t == 1){
+    printf("TIME DEPENDENT ELECTRIC FIELD ACTIVATED \n");
+  }
   
 
   char    filename[FILENAME_MAX];
